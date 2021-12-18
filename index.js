04529163a51871e5d83138c674b4616f8e419d1a
evazhu <eva628@live.unc.edu>
2021-12-17 21:40:47 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req, res) => {
+  res.send({hi: 'there' });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
+
+//underlysing express server - app
+//by calling app.get  to create a handler
+//the request wants to  get/post/put/delete/patch
+//req object representing the incoming request
+//res object representing the outgoing response
+//res.send immediately send some JSON back to who ever made this request
