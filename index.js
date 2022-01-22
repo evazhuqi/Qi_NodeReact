87e5c94c2f23073c9617301b62581d5d4b590ee9
evazhu <eva628@live.unc.edu>
2022-01-21 23:01:07 -0500

    Added billing and client

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport'); //tell passport to make use of cookie
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 
 require('./models/User');
 require('./services/passport');
 mongoose.connect(keys.mongoURI);
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(
   cookieSession({
@@ -22,17 +25,29 @@ app.use(passport.session());
 
 
 require('./routes/authRoutes')(app);
-
+require('./routes/billingRoutes')(app);
 
 //app.get('/',(req, res) => {
 //  res.send({ bye: 'buddy' });
 //});
 
-
+if (process.env.NODE_ENV === 'production') {
+  // express will serve up production assets
+  // like our main.js ifle or main.css file
+  app.use(express.static('client/build'));
+  // express will serve up the index.html filled
+  // if it does not recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+
 
 //underlysing express server - app
 //by calling app.get  to create a handler
